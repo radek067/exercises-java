46a6ef728d4102a86679e8b16f4559ad31c95cef
radek067 <wwx27300@student.warszawa.merito.pl>
2024-12-21 08:51:09 +0100

    Add files via upload

---
+++ exercises-java/App/Company.java
@@ -0,0 +1,37 @@
+package App;
+
+import java.util.List;
+
+public class Company {
+    private String name;
+    private Address address;
+    private List<Employee> employees;
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ exercises-java/App/Utils.java
@@ -0,0 +1,54 @@
+package App;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Utils {
+    static int inputInt(String message) {
+        try {
+            System.out.print(message);
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Podano błędną wartość");
+            return inputInt(message);
+        }
+    }
+
+    static String inputString(String message) {
+        System.out.print(message);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    static void printBanner(String title) {
+        String border = "+" + "-".repeat(title.length() + 2) + "+";
+
+        System.out.println("\n" + border);
+        System.out.println("| " + title.toUpperCase() + " |");
+        System.out.println(border + "\n");
+    }
+
+    static void printTable(List<String> collection, String title) {
+        int titleLength = title.length();
+        int longestItemLength = collection.stream().mapToInt(String::length).max().orElse(0);
+        int columnLength = Math.max(titleLength, longestItemLength);
+        String titlePlaceholder = "| Id | %-" + columnLength + "s |%n";
+        String itemPlaceholder = "| %-2d | %-" + columnLength + "s |%n";
+        String horizontalBorder = "+----+" + "-".repeat(columnLength + 2) + "+";
+
+        System.out.println(horizontalBorder);
+        System.out.printf(titlePlaceholder, title);
+        System.out.println(horizontalBorder);
+        for (int i = 0; i < collection.size(); i++) {
+            System.out.printf(itemPlaceholder, i + 1, collection.get(i));
+            //            System.out.println(horizontalBorder);
+        }
+        System.out.println(horizontalBorder);
+    }
+
+    static void printSeparator() {
+        System.out.println("--------------------------------");
+    }
+}
